756e91d710cb5db150ed897f5bdb6c00e0634fa5
DI2uNk <jd.drunk@gmail.com>
2013-04-14 23:52:16 +0200

    Resolved errors with assets compile process

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ Juandediosherrero::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( curriculum/application_curriculum.js curriculum/application_curriculum.css )
+  #Curriculum assets
+  config.assets.precompile += %w( curriculum/print.css curriculum/ie7Fix.css curriculum/DD_belatedPNG_0.0.7a-min.js curriculum/ieFix.js curriculum/ieFix.css curriculum/application_curriculum.js curriculum/application_curriculum.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
